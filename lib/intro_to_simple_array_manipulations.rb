e8b1c18a203a0041c571d007780b405cfd4c2c83
Lourenz Magoncia <ljmagoncia@yahoo.com>
2019-07-05 05:56:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,3 +33,15 @@ end
 def using_uniq(array)
   array.uniq
 end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.uniq
+end
+
+def using_uniq(array)
+  array.uniq
+end
